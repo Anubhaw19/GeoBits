68f786f4a5bc7be3da8c45d333d9f7e101552677
Anubhaw19 <anubhawsharma99@gmail.com>
2020-03-17 19:54:29 +0530

    animation added

--- app/src/main/java/com/example/geology/start.java
+++ app/src/main/java/com/example/geology/start.java
@@ -6,14 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class start extends AppCompatActivity {
-
+    ImageView img;
+    LinearLayout linearLayout;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+       linearLayout=(LinearLayout) findViewById(R.id.linear);
+        textView=(TextView)findViewById(R.id.textView);
+        img=(ImageView)findViewById(R.id.icon);
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation=new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(1500);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        img.setAnimation(animation);
+
+
+
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -21,7 +44,7 @@ public class start extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
-        }, 2000); //3000 L = 3 detik
+        }, 2500); //3000 L = 3 detik
 
 //        TextView txt=(TextView)findViewById(R.id.textView);
 //        txt.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +53,26 @@ public class start extends AppCompatActivity {
 //                Intent intent=new Intent(start.this,MainActivity.class);
 //                startActivity(intent); }
 //        });
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener
+    {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            img.clearAnimation();
+            img.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
